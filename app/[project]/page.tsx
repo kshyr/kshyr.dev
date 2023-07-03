871c9abbf29306da4c6ca1b4d7366f687610f332
deko95 <ks1379@scarletmail.rutgers.edu>
2023-07-02 20:42:47 -0400

    test if helps with loading times

--- app/[project]/page.tsx
+++ app/[project]/page.tsx
@@ -1,9 +1,27 @@
-import { projects } from "../projects";
-export default function Page({ params }: { params: { project: string } }) {
+import { Project } from "@/lib/types";
+import { projects, getREADME } from "../projects";
+
+// @ts-expect-error
+async function getMarkdown(project: Project) {
+  if (project.bodyMarkdown) {
+    return project.bodyMarkdown;
+  }
+  const markdown = await getREADME(project.handle);
+
+  return markdown;
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { project: string };
+}) {
   const project = projects.find((i) => i.handle === params.project);
+  const markdown = getMarkdown(project as Project);
+
   return (
     <div>
-      My Post: {JSON.stringify(project)} {project?.bodyMarkdown}
+      My Post: {JSON.stringify(project)} {markdown}
     </div>
   );
 }
